aee92675cda390571a6b448fdfa005be2681fc15
Bhabaranjan19966 <bhabaranjanpanigrahi@gmail.com>
2018-09-11 13:58:24 +0530

    usign native node library

--- file.js
+++ file.js
@@ -1,38 +1,77 @@
-var csv = require('fast-csv');
-var fs = require('fs');
-var stream = fs.createReadStream("Crimes_-_2001_to_present.csv");
-var v = [];
-var myMap = new Map();
-var mySet = new Set();
-csv
-    .fromStream(stream, { headers: [, , , , , "Primary_Type", "Description", , , , , , , , , , , "Year", , , , ,] })
-    .on("data", function (data) {
-        if (data.Primary_Type == "ROBBERY") {
-            var previousSize = mySet.size;
-            mySet.add(data.Description);
-            var afterSize = mySet.size;
-            //checking if it is a new type or robbery
-            if (previousSize < afterSize) {
-                myMap.set(data.Description, 1);
-            }
-            else {
-                var previousValue = myMap.get(data.Description);
-                myMap.set(data.Description, previousValue + 1);
-                console.log(previousValue + "prev1");
-                console.log((previousValue + 1) + "prev2");
+var fs = require('fs'),
+    readline = require('readline'),
+    stream = require('stream');
+
+var instream = fs.createReadStream('Crimes_-_2001_to_present.csv');
+var outstream = new stream;
+outstream.readable = true;
+outstream.writable = true;
+
+var rl = readline.createInterface({
+    input: instream,
+    output: outstream,
+    terminal: false
+});
+var rob = new Array(17);
+var burg = new Array(17);
+rob.fill(0);
+burg.fill(0);
+console.log(burg[5]);
+rl.on('line', function(line) {
+    if(line.search('ROBBERY')){
+        var s = line.split(",");
+        var index;
+        for(i = 0 ; i<s.length;i++){
+            if(Number(s[i])>2000 && Number(s[i])<2017){
+                index=Number(s[i]);
+                break;
             }
         }
-    })
-    .on("end", function () {
-        var iterator1 = myMap[Symbol.iterator]();
-        for (let item of iterator1) {
-            var prop = {};
-            prop[item[0]] = item[1];
-            v.push(prop);
-        }
-        fs.writeFileSync('2end.json', JSON.stringify(v), (err) => {
-            if (err) {
-                console.log('error while parsing');
+        index -= 2001;
+        rob[index] +=1;
+
+        console.log( "rob " +index+ "--------------" + "------" + rob[index] );
+    }
+    if(line.search('BURGLARY')){
+        var s = line.split(",");
+        var index;
+        for(i = 0 ; i<s.length;i++){
+            if(Number(s[i])>2000 && Number(s[i])<2017){
+                index=Number(s[i]);
+                break;
             }
-        });
+        }
+        index -= 2001;
+        burg[index] +=1;
+        console.log( "burg " +index+ "--------------" + "------" + burg[index] );
+    }
+
+});
+rl.on('close',()=>{
+    console.log("file ----- closed");
+    var v =[];
+    var obj={
+        Primary_Type:"ROBBERY",
+        Year:"",
+        frequency:0,
+    };
+    for(i=0 ; i <burg.length;i++){
+        var year = 2000 + i+1 ;
+        obj.Year = year;
+        obj.frequency = burg[i];
+        v.push(obj);
+        console.log(obj);
+    }
+    obj.Primary_Type="BURGLARY";
+    for(i=0 ; i <rob.length;i++){
+        var year = 2000 + i+1;
+        obj.Year = year;
+        obj.frequency = rob[i];
+        v.push(obj);
+    }
+    fs.writeFileSync('1st.json' , JSON.stringify(v), (err)=> {
+        if(err){
+            console.log('file not found');
+        }
     });
+})
