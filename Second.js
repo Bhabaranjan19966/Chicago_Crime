0b935d9b28037dd9e80840e4c858c8f377edd266
Bhabaranjan19966 <bhabaranjanpanigrahi@gmail.com>
2018-09-11 20:09:35 +0530

    generated json for 2nd problem statement

--- Second.js
+++ Second.js
@@ -13,13 +13,80 @@ var rl = readline.createInterface({
     terminal: false
 });
 var vechile = new Array(16);
-var property= new Array(16);
-var stateup= new Array(16);
+var property = new Array(16);
+var stateup = new Array(16);
 vechile.fill(0);
 property.fill(0);
 stateup.fill(0);
-rl.on('line', (line)=>{
-    if(line.search('Criminal Damage')){
+const hasvechile = /TO PROPERTY/;
+const hasprop = /TO VEHICLE/;
+const hasstate = /TO STATE SUP PROP/;
+const has = /CRIMINAL DAMAGE/;
+rl.on('line', (line) => {
+    if (has.test(line)) {
+
+        if (hasvechile.test(line)) {
+            var index;
+            var x = line.split(",");
+            for (i = 0; i < x.length; i++) {
+                if (Number(x[i]) > 2000 && Number(x[i]) < 2017) {
+                    index = Number(x[i]);
+                    break;
+                }
+            }
+            vechile[index - 2001] += 1;
+            //console.log(index+"     "+"vechile");
+        }
+        if (hasstate.test(line)) {
+            var index;
+            var x = line.split(",");
+            for (i = 0; i < x.length; i++) {
+                if (Number(x[i]) > 2000 && Number(x[i]) < 2017) {
+                    index = Number(x[i]);
+                    break;
+                }
+            }
+            stateup[index - 2001] += 1;
+            //console.log(index+"     "+"state");
+        }
+        if (hasprop.test(line)) {
+            var index;
+            var x = line.split(",");
+            for (i = 0; i < x.length; i++) {
+                if (Number(x[i]) > 2000 && Number(x[i]) < 2017) {
+                    index = Number(x[i]);
+                    break;
+                }
+            }
+            property[index - 2001] += 1;
+            //console.log(index+"     "+"prop");
+        }
+        
         
     }
+})
+rl.on("close" , () => {
+    var v = [];
+    for(i=0; i<vechile.length;i++){
+        
+        var type ={
+            Property:"",
+            Vechile:"",
+            State:""
+        }
+        var obj={
+            Year:"",
+            Types:type,
+        }
+        type.Property=property[i];
+        type.State=stateup[i];
+        type.Vechile=vechile[i];
+        obj.Year= 2000+i+1;        
+        v.push(obj);
+       // console.log(obj);
+    }
+    fs.writeFileSync('assignment2.json', JSON.stringify(v),(err)=>{
+        console.log("error while writing");
+    })
+
 })
--- file.js
+++ file.js
@@ -14,66 +14,65 @@ var rl = readline.createInterface({
 });
 var rob = new Array(16);
 var burg = new Array(16);
-var v =[];
+var v = [];
 rob.fill(0);
 burg.fill(0);
-console.log(burg[5]);
-var ln=0;
-rl.on('line', function(x) {
+// console.log(burg[5]);
+var ln = 0;
+rl.on('line', function (x) {
     ln++;
-    if(x.search('ROBBERY')!=-1)
-    {   
-        
-        console.log(ln);
+    if (x.search('ROBBERY') != -1) {
+
+        // console.log(ln);
         var s = x.split(",");
         var index;
-        for(i = 0 ; i<s.length;i++){
-            if(Number(s[i])>2000 && Number(s[i])<2017){
-                index=Number(s[i]);
-                console.log(s[i]);
+        for (i = 0; i < s.length; i++) {
+            if (Number(s[i]) > 2000 && Number(s[i]) < 2017) {
+                index = Number(s[i]);
+                // console.log(s[i]);
                 break;
             }
         }
         index -= 2001;
-        rob[index] +=1;
-        console.log( "rob " +index+ "--------------" + "------" + rob[index] );
+        rob[index] += 1;
+        // console.log("rob " + index + "--------------" + "------" + rob[index]);
     }
-     if(x.search('BURGLARY') != -1){
+    if (x.search('BURGLARY') != -1) {
         var s = x.split(",");
         var index;
-        for(i = 0 ; i<s.length;i++){
-            if(Number(s[i])>2000 && Number(s[i])<2017){
-                index=Number(s[i]);
+        for (i = 0; i < s.length; i++) {
+            if (Number(s[i]) > 2000 && Number(s[i]) < 2017) {
+                index = Number(s[i]);
                 break;
             }
         }
         index -= 2001;
-        burg[index] +=1;
-        console.log( "burg " +index+ "--------------" + "------" + burg[index] );
+        burg[index] += 1;
+        // console.log("burg " + index + "--------------" + "------" + burg[index]);
     }
 
 });
-rl.on('close',()=>{
-    console.log("file ----- closed");
-    
-    
-    for(i=0 ; i <burg.length;i++){
-        var obj={
-        YEAR:"",
-        ROBBERY:0,
-        BURGLARY:0,
-         };
-        var year = 2000 + i+1 ;
+rl.on('close', () => {
+    // console.log("file ----- closed");
+
+
+    for (i = 0; i < burg.length; i++) {
+        var obj = {
+            YEAR: "",
+            ROBBERY: 0,
+            BURGLARY: 0,
+        };
+        var year = 2000 + i + 1;
         obj.YEAR = year;
         obj.BURGLARY = burg[i];
-        obj.ROBBERY=rob[i];
+        obj.ROBBERY = rob[i];
         v.push(obj);
-        console.log(obj);
+        // console.log(obj);
     }
-    console.log(v);
-    fs.writeFileSync('1st.json' , JSON.stringify(v), (err)=> {
-        if(err){
-            console.log('file not found');
+    // console.log(v);
+    fs.writeFileSync('1st.json', JSON.stringify(v), (err) => {
+        if (err) {
+            // console.log('file not found');
         }
     });
 })
